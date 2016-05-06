c2d4ea1b5a9d73fa6f12d8c4afc4a75e979b4a80
Chunqi SHI <chqshi@gmail.com>
2016-05-06 21:34:45 +0800

    added categories

---
+++ src/main/java/edu/brandeis/cs/planner/deduction/Facts.java
@@ -0,0 +1,16 @@
+package edu.brandeis.cs.planner.deduction;
+
+/**
+ * Created by 310201833 on 2016/5/6.
+ */
+public class Facts {
+
+    public static enum Categories {Text, Splitter, Tokenizer, Tagger, Chunking, NER, Coreference}
+
+
+    public boolean isCategory(String name, Categories cat) {
+
+        return false;
+    }
+    ;
+}
