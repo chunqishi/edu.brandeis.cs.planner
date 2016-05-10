3804e8a772fec32dd3212cf972b90c23fef88da2
Chunqi SHI <chqshi@gmail.com>
2016-05-10 18:22:52 +0800

    remove credential

--- src/main/java/edu/brandeis/cs/planner/utils/URLFetcher.java
+++ src/main/java/edu/brandeis/cs/planner/utils/URLFetcher.java
@@ -59,16 +59,16 @@ public class URLFetcher {
                 URL url = new URL(urlString);
                 String host = url.getHost();
                 int port = url.getPort();
-                System.out.println(
-                        "connection/credentials/credential[@host='" + host + "'and @port='" + port + "']/username");
                 String username = ConfigXML.config().getString(
                         "connection/credentials/credential[@host='" + host + "' and @port='" + port + "']/username");
                 String password = ConfigXML.config().getString(
                         "connection/credentials/credential[@host='" + host + "' and @port='" + port + "']/password");
                 logger.debug("Credential: Username = '{}'", username);
-                credsProvider.setCredentials(
-                        new AuthScope(host, port),
-                        new UsernamePasswordCredentials(username, password));
+                if(username != null) {
+                    credsProvider.setCredentials(
+                            new AuthScope(host, port),
+                            new UsernamePasswordCredentials(username, password));
+                }
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
