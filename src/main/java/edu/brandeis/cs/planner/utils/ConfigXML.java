c5aafb4547893ac9bf7b287b779ec6de63239510
Chunqi SHI <chqshi@gmail.com>
2016-05-10 19:06:24 +0800

    fix xml config path

--- src/main/java/edu/brandeis/cs/planner/utils/ConfigXML.java
+++ src/main/java/edu/brandeis/cs/planner/utils/ConfigXML.java
@@ -5,11 +5,16 @@ import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.tree.xpath.XPathExpressionEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URISyntaxException;
 
 /**
  * Created by 310201833 on 2016/5/6.
  */
 public class ConfigXML {
+    final static Logger logger = LoggerFactory.getLogger(ConfigXML.class);
     XMLConfiguration config = null;
     Parameters params = new Parameters();
     static ConfigXML self = null;
@@ -25,10 +30,20 @@ public class ConfigXML {
     }
 
     protected ConfigXML() {
-        this(ConfigXML.class.getResource("/config.xml").getPath());
+        try {
+            String xmlPath = ConfigXML.class.getResource("/config.xml").toURI().getPath();
+            init(xmlPath);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     public ConfigXML(String xmlPath) {
+        init(xmlPath);
+    }
+
+    protected void init(String xmlPath) {
+        logger.debug("XML_PATH:{}", xmlPath);
         FileBasedConfigurationBuilder<XMLConfiguration> builder =
                 new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
                         .configure(params.xml().setFileName(xmlPath)
