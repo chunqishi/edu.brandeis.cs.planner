499c797df8e722334ad63a56cab897fca7208712
Chunqi SHI <chqshi@gmail.com>
2016-05-10 13:59:52 +0800

    added

---
+++ src/main/resources/planner_template_1.pl
@@ -0,0 +1,76 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%%  Libraries
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% use_module(library(clpfd)).
+% lib(clpfd).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%%  Facts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+splitter(a1).
+splitter(b1).
+
+tokenizer(a2).
+tokenizer(b2).
+
+tagger(a3).
+tagger(b3).
+
+chunking(a4).
+chunking(b4).
+
+ner(a5).
+ner(b5).
+
+parser(a6).
+parser(b6).
+
+dependencyparser(a7).
+dependencyparser(b7).
+
+
+coreference(a8).
+coreference(b8).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%%  Rules
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+workflow(A,B,2,[A,B]):-
+    splitter(A),
+    tokenizer(B).
+
+workflow(A,B,2,[A,B]):-
+    tokenizer(A),
+    tagger(B).
+
+workflow(A,B,2,[A,B]):-
+    tagger(A),
+    parser(B).
+
+workflow(A,B,2,[A,B]):-
+    tagger(A),
+    dependencyparser(B).
+
+workflow(A,B,2,[A,B]):-
+    tagger(A),
+    coreference(B).
+
+workflow(A,B,2,[A,B]):-
+    tagger(A),
+    ner(B).
+
+workflow(A,C,N,[A,B|L]) :-
+	length([A,B|L],N),
+	length([B|L],M),
+	M >= 2,
+    workflow(A,B,2,[A,B]),
+    workflow(B,C,M,[B|L]).
+		
+
+
+
+
---
+++ src/main/resources/planner_template_2.pl
@@ -0,0 +1,68 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%%  Facts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+splitter(a1).
+splitter(b1).
+
+tokenizer(a2).
+tokenizer(b2).
+
+tagger(a3).
+tagger(b3).
+
+chunking(a4).
+chunking(b4).
+
+ner(a5).
+ner(b5).
+
+parser(a6).
+parser(b6).
+
+dependencyparser(a7).
+dependencyparser(b7).
+
+
+coreference(a8).
+coreference(b8).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%%  Rules
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+pipeline(A,B):-
+    splitter(A),
+    tokenizer(B).
+
+pipeline(A,B):-
+    tokenizer(A),
+    tagger(B).
+
+pipeline(A,B):-
+    tagger(A),
+    parser(B).
+
+pipeline(A,B):-
+    tagger(A),
+    dependencyparser(B).
+
+pipeline(A,B):-
+    tagger(A),
+    coreference(B).
+
+pipeline(A,B):-
+    tagger(A),
+    ner(B).
+
+workflow(A,B,[A,B]):-
+    pipeline(A,B).
+
+workflow(A,C,[A,B|L]):-
+    pipeline(A,B),
+    workflow(B,C,[B|L]).
+	
+
+
+
+
