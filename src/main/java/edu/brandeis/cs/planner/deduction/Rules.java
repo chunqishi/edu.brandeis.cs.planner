6c2b3a68ef9d61137cc82d28ff4942f2b589c998
Chunqi SHI <chqshi@gmail.com>
2016-05-10 19:30:59 +0800

    remove grid

--- src/main/java/edu/brandeis/cs/planner/deduction/Rules.java
+++ src/main/java/edu/brandeis/cs/planner/deduction/Rules.java
@@ -6,14 +6,12 @@ import org.apache.commons.io.IOUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Rules {
-
     private String scriptPath = "/planner.pl";
-
-
     private List<String> rules = new ArrayList<>();
 
     public List<String> getRules() {
@@ -29,6 +27,13 @@ public class Rules {
     }
 
     public static String getFileAsString(String path) throws IOException {
-        return IOUtils.toString(new FileInputStream(new File(Rules.class.getResource(path).getFile())));
+        String scriptPath = null;
+        try {
+            scriptPath = Rules.class.getResource(path).toURI().getPath();
+            return IOUtils.toString(new FileInputStream(new File(scriptPath)));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
